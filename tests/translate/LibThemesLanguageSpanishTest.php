4b1da9b58639ecbb625a7d2dbb3a04eea5d51f0c
Randler Ferraz Almeida <randler.ferraz@codificar.com.br>
2023-09-22 16:35:20 -0300

    correção teste tradução es-py

--- tests/translate/LibThemesLanguageSpanishTest.php
+++ tests/translate/LibThemesLanguageSpanishTest.php
@@ -19,7 +19,7 @@ class LibThemesLanguageSpanishTest extends TestCase
         
         $path = base_path() . '/vendor/codificar/themes/src/translations';
         $directoryPt = "$path/pt-br/*.php";
-        $directoryEs = "$path/es/*.php";
+        $directoryEs = "$path/es-py/*.php";
         $filesPt = glob($directoryPt);
         $filesEs = glob($directoryEs);
 
