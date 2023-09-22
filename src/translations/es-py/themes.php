b528190b2e3224165c45c8cf64cb26b002f625eb
Randler Ferraz Almeida <randler.ferraz@codificar.com.br>
2023-09-22 16:19:10 -0300

    tradução es-py adicionado

---
+++ src/translations/es-py/themes.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'themes' => 'Temas',
+    'id' => 'ID',
+    'theme_color' => 'Color del Tema',
+    'primary_color' => 'Color Primario',
+    'secondary_color' => 'Color Secundario',
+    'hover_color' => 'Color al Pasar el Cursor',
+    'active_color' => 'Color Activo',
+    'actions' => 'Acciones',
+    'apply' => 'Aplicar este Tema',
+    'delete' => 'Borrar',
+    'edit' => 'Editar',
+    'new' => 'Nuevo',
+    'choose' => 'Elegir una Imagen',
+    'save' => 'Guardar',
+    'options' => 'Opciones',
+    'yes' => 'Sí',
+    'no' => 'No',
+    'select_options' => 'Permitir selección de tema en las aplicaciones',
+    'menu_name' => 'Nombre del Menú de Configuración',
+    'menu_frase' => 'Frase del Tema del Menú de Configuración',
+    'is_visible_in_app' => 'Permitir visibilidad en la aplicación',
+    'app_image_icon' => 'Imagen a utilizar en la aplicación',
+    'success' => 'Opción guardada exitosamente',
+    'theme_name' => 'Nombre del Tema',
+    'error' => '¡Completa todos los colores para finalizar la edición!'
+];
